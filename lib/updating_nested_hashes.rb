05d071fdc93d65ab3637f229678962cf888be01d
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 23:58:35 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,15 +1,15 @@
 def update_status
   epic_tragedy = {
     :montague => {
-        :patriarch => {name: "Lord Montague", age: "53"},
-        :matriarch => {name: "Lady Montague", age: "54"},
-        :hero => {name: "Romeo", age: "15", status: "alive"},
-        :hero_friends => [
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   },
-   :capulet => {
+    },
+    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
@@ -19,4 +19,9 @@ def update_status
       ]
     }
   }
+  
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
+  epic_tragedy
 end
